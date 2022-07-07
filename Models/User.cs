5da62da68b57c8959e256a9570d1117ce5fc627b
Shawn Penning <penningsp@hotmail.com>
2022-07-07 19:57:18 -0400

    Added class and migrated to db

--- Models/User.cs
+++ Models/User.cs
@@ -9,7 +9,7 @@
 		public string UserName { get; set; }
 		public string Password { get; set; }
 		public string ZipCode { get; set; } // Default location; can be changed by user anytime.
-		public string Key { get; set; } // This is returned by a call to LocationFinder API where zip code is sent. Key is used by Current Weather API.
+		// public string Key { get; set; } // This is returned by a call to LocationFinder API where zip code is sent. Key is used by Current Weather API.
 		public string Activity { get; set; } // Consider making this an enum. To start, only include Run, Walk. User can edit and create more, so enum may not be suitable.
 		public int OutfitScaleOffset { get; set; } // negative or positive number to adjust the standard OutfitScale score by.
 	}
---
+++ Models/ZipCodeKey.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProjApi.Models
+{
+    public class ZipCodeKey
+    {
+        public int Id { get; set; }
+        public string ZipCode { get; set; }
+        public string Key { get; set; }
+    }
+}
