938dacf1829a6ab1212fb448d3bb9a1ced2f122e
Malik El Guermai <malikelguermai@outlook.com>
2022-07-11 20:58:02 -0400

    OutfitWeather

---
+++ Services/OutfitWeatherService.cs
@@ -0,0 +1,25 @@
+﻿using FinalProjApi.Data;
+using FinalProjApi.Models;
+using FinalProjApi.Services.Interfaces;
+
+namespace FinalProjApi.Services
+
+{
+    public class OutfitWeatherService : IOutfitWeatherService
+    {
+        private readonly FinalProjectDBContext _context;
+
+        public OutfitWeatherService(FinalProjectDBContext context)
+        {
+            _context = context;
+        }
+
+        public List<OutfitWeather> GetCurrentWeatherOutfit(int currentTemperature)
+        {
+            var defaultOutfit = _context.Outfits.Where(x => x.MinTemperature <= currentTemperature && x.MaxTemperature >= currentTemperature).ToList();
+
+            return defaultOutfit;
+
+        }
+    }
+}
