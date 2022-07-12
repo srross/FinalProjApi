a2d9db2047a2104e7ddaa555e8ee528b9de6e386
SoniaRoss <srross99@gmail.com>
2022-07-11 21:16:03 -0400

    add migrations

---
+++ Migrations/20220711230904_changeMinAndMaxTempColumnsDataType.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProjApi.Migrations
+{
+    public partial class changeMinAndMaxTempColumnsDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "MinTemperature",
+                table: "Outfits",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "MaxTemperature",
+                table: "Outfits",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinTemperature",
+                table: "Outfits",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxTemperature",
+                table: "Outfits",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
