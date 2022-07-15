f00c8b5eb7bf97f5e576dac032ec38c7d39d9620
SoniaRoss <srross99@gmail.com>
2022-07-14 20:53:33 -0400

    Add Post(create), Update(put), and Delete endpoints to OutfitWeatherController

--- Services/Interfaces/IOutfitWeatherService.cs
+++ Services/Interfaces/IOutfitWeatherService.cs
@@ -5,6 +5,10 @@ namespace FinalProjApi.Services.Interfaces
     public interface IOutfitWeatherService
     {
         Task<List<OutfitWeather>> GetCurrentWeatherOutfit(double currentTemperature);
+        void AddOutfitToUserProfile(OutfitWeather outfit);
+        void DeleteUserOutfit(int id);
+        void UpdateUserOutfit(int id, OutfitWeather outfitWeather);
+
         //Task<List<OutfitWeather>> GetAllOutfitsByUserId(int userId);
     }
 }
--- Services/OutfitWeatherService.cs
+++ Services/OutfitWeatherService.cs
@@ -1,6 +1,7 @@
 ﻿using FinalProjApi.Data;
 using FinalProjApi.Models;
 using FinalProjApi.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinalProjApi.Services
 
@@ -20,8 +21,65 @@ namespace FinalProjApi.Services
 
             return defaultOutfit;
         }
-        
-        // Cleanup later
+
+        public async void AddOutfitToUserProfile(OutfitWeather outfit)
+        {
+            if (_context.Outfits == null)
+            {
+                //return Problem("Entity set 'FinalProjectDBContext.Outfits'  is null.");
+            }
+            _context.Outfits.Add(outfit);
+
+            _context.SaveChangesAsync();
+        }
+
+        public void DeleteUserOutfit(int id)
+        {
+            var outfit = _context.Outfits.Find(id);
+
+            if (outfit == null)
+            {
+                //return NotFound();
+            }
+
+            _context.Outfits.Remove(outfit);
+
+            _context.SaveChangesAsync();
+        }
+
+        public void UpdateUserOutfit(int id, OutfitWeather outfitWeather)
+        {
+            if (id != outfitWeather.Id)
+            {
+                //return "BadRequest";
+            }
+
+            _context.Entry(outfitWeather).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OutfitWeatherExists(id))
+                {
+                    //return "NotFound";
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            //return "Looks like it worked.";
+        }
+
+        private bool OutfitWeatherExists(int id)
+        {
+            return (_context.Outfits?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
         //public async Task<List<OutfitWeather>> GetAllOutfitsByUserId(int userId)
         //{
         //    var userOutfits = _context.Outfits.Where(x => x.UserId == userId).ToList();
