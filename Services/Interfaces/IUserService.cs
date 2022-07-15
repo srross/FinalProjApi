4ea0fd76a48ccc83512a57e55e18fb42f5901baa
SoniaRoss <srross99@gmail.com>
2022-07-14 23:10:47 -0400

    Add UserController, UserService, and IUserservice

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using FinalProjApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProjApi.Services.Interfaces
+{
+    public interface IUserService
+    {
+        void AddNewUser(User user);
+
+        User GetUserById(int id);
+    }
+}
